5a75d9d8cf78ab75f2db9f564350cc05db85c7fb
neoneoneo1 <af1234@mail.ru>
2011-10-26 15:33:05 +0400

    и поток для перемещения добавим

--- java_proj/chemiLogik.java
+++ java_proj/chemiLogik.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class chemiLogik
 {
+    private static int cnt=0;
+    //Проверка завершения игры
     public static boolean isFinished(int mas[][],int etalon[][])
     {
         int i,j,r,c;
@@ -45,6 +48,7 @@ public class chemiLogik
         return false;
     }
 
+    //Вывод массива
     public static void out(int out[][])
     {
         int i,j;
@@ -54,80 +58,118 @@ public class chemiLogik
                 System.out.print(out[i][j]);
             System.out.print("\n");
         }
+        System.out.print("\n");
+    }
+
+    //Получение индекса позиции куда ещё можно доехать чтоб упереться
+    //0-вправо, 1-влево, 2-вверх,3-вниз
+    public static int getStepCount(int curRow,int curCol,int direction, int mas[][])
+    {
+        int i=0;
+        int colCount=mas[0].length;
+        int rowCount=mas.length;
+        if(direction==0 && curCol!=(colCount-1))//вправо и фокус стоит не справа скраю
+        {
+            //движение горизонтальное по колонкам вправо
+            for(i=curCol+1;i<colCount;i++)
+                if(mas[curRow][i]!=0)
+                    return i-1;
+        }
+        if(direction==1 && curCol!=0)//влево
+        {
+            //движение горизонтальное по колонкам вправо
+            for(i=curCol-1;i>=0;i--)
+                if(mas[curRow][i]!=0)
+                    return i+1;
+        }
+        if(direction==2 && curRow!=0)//вверх
+        {
+            //движение вертикальное по строкам вниз
+            for(i=curRow-1;i>=0;i--)
+                if(mas[i][curCol]!=0)
+                    return i+1;
+
+        }
+        if(direction==3 && (curRow!=rowCount-1))//вниз
+        {
+            //движение вертикальное по строкам вниз
+            for(i=curRow+1;i<rowCount;i++)
+                if(mas[i][curCol]!=0)
+                    return i-1;
+        }
+        return 0;
+    }
+
+    public static int makeStep(int curRow,int curCol,int stepPosRow, int spepPosCol, int mas[][])
+    {
+        int karman=mas[curRow][curCol];
+        mas[curRow][curCol]=mas[stepPosRow][spepPosCol];
+        mas[stepPosRow][spepPosCol]=karman;
+        return 0;
     }
 
     public static void main(String[] pars)
     {
-        int [][] mas={      {1,0,0,0,0},
+        int [][] mas={      {2,0,0,0,0},//2ка это припятствие
                             {0,0,0,0,0},
                             {0,0,0,0,1},
                             {0,0,0,0,1},
-                            {0,0,0,0,1}
+                            {1,0,0,0,1}
                         };
         int [][] etalon={   {0,1},
                             {1,1},
                             {0,1},
                         };
+//        out(mas);
+//        out(etalon);
 
-
-        out(mas);
-        out(etalon);
         //нажатие вправо
-
         //0-вправо, 1-влево, 2-вверх,3-вниз
-        int direction=0;
-        if(isFinished(mas,etalon))
-            System.out.println("ok");
-        else
-            System.out.println("error");
+//        int direction=0;
+//        if(isFinished(mas,etalon))
+//            System.out.println("ok");
+//        else
+//            System.out.println("error");
+
+        //двигаем вверх
+        out(mas);
+        makeStep(4,0,getStepCount(4,0,2, mas),0,mas);
+        out(mas);
 
 
+
+        //Потоки 1й способ создания
+        //Создание потока путем наследования класса Thread
+        class thrd extends Thread
+        {
+            thrd(){}
+            @Override
+            public void run()
+            {
+                try
+                {
+                    for(int i=0;i<5;i++)
+                    {
+                        sleep(1000);
+                        System.out.print(cnt);
+                        cnt++;
+                    }
+                }
+                catch (InterruptedException e) { return; }
+                System.out.print("\n");
+            }
+        }
+
+        thrd c=new thrd();
+        c.start();
+        //Остановить основной (вызывающий) поток и ждать пока не завершиться вызванный поток
+        try
+        {
+            c.join();
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println("catch p.t.join()");
+        }
     }
-
-
-//дальше просто вывод
-//        //Нашли вхождение и снего проверить соответствует ли матрица подматрице
-//        //подбор шаблона по игровой матрице
-//        for(i=0;i<etalon.length;i++)
-//        {
-//            for(j=0;j<etalon[i].length;j++)
-//            {
-//                System.out.print(etalon[i][j]);
-//            }
-//            System.out.print("\n");
-//        }
-//
-//
-//        //Перебор игровой матрицы
-//        for(r=0;r<mas.length;r++)
-//        {
-//            for(c=0;c<mas[r].length;c++)
-//            {
-////                //Нашли вхождение и снего проверить соответствует ли матрица подматрице
-////                //подбор шаблона по игровой матрице
-////                if(r<(mas.length-etalon.length) && c<(mas[0].length-etalon[0].length))
-////                {
-////                    boolean bFind=true;
-////                    Outr:
-////                    {
-////                        for(i=0;i<etalon.length;i++)
-////                        {
-////                            for(j=0;j<etalon[i].length;j++)
-////                            {
-////                                if(!(etalon[i][j]==1 && etalon[i][j]==mas[r+i][c+j]))
-////                                {
-////                                    bFind=false;
-////                                    break Outr;
-////                                }
-////                            }
-////                        }
-////                    }
-////                    if(bFind)
-////                        System.out.println("ok");
-////                }
-//                System.out.print(mas[r][c]);
-//            }
-//            System.out.print("\n");
-//        }
-//    }
 }
